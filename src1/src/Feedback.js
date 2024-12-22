9dad7cf2381864c597293013f2ea9d1f40aa0c7a
Sourajit Nayak <sourajitnayak3471@gmail.com>
2024-12-23 02:04:48 +0530

    Update Feedback.js

--- src1/src/Feedback.js
+++ src1/src/Feedback.js
@@ -17,7 +17,7 @@ const Feedback = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/feedback', {
+      const response = await fetch('https://mcqgeneratorapp.onrender.com/api/feedback', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
